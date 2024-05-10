2b99b62939ce718ff79c1c01722b348adfeea9db
ArdaMertKeles <ardakelesmert@gmail.com>
2024-05-10 23:39:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,700 @@
+var words = [
+    {
+        eng: 'Source',
+        tr: 'kaynak',
+    },
+    {
+        eng: 'Door',
+        tr: 'kapı',
+    },
+    {
+        eng: 'Left',
+        tr: 'sol',
+    },
+    {
+        eng: 'Decision',
+        tr: 'karar',
+    },
+    {
+        eng: 'Vote',
+        tr: 'oy',
+    },
+    {
+        eng: 'Bottle',
+        tr: 'şişe',
+    },
+    {
+        eng: 'Adopt',
+        tr: 'evlat edinmek',
+    },
+    {
+        eng: 'Agree',
+        tr: 'katılmak',
+    },
+    {
+        eng: 'Smile',
+        tr: 'gülümsemek',
+    },
+    {
+        eng: 'Behave',
+        tr: 'davranış',
+    },
+    {
+        eng: 'Nation',
+        tr: 'ulus',
+    },
+    {
+        eng: 'Patience',
+        tr: 'sabır',
+    },
+    {
+        eng: 'Measure',
+        tr: 'tedbir',
+    },
+    {
+        eng: 'Point',
+        tr: 'nokta',
+    },
+    {
+        eng: 'Spy',
+        tr: 'casus',
+    },
+    {
+        eng: 'Data',
+        tr: 'veri',
+    },
+    {
+        eng: 'Wood',
+        tr: 'odun',
+    },
+    {
+        eng: 'Busy',
+        tr: 'meşgul',
+    },
+    {
+        eng: 'Bush',
+        tr: 'çalı',
+    },
+    {
+        eng: 'Brush',
+        tr: 'fırça',
+    },
+    {
+        eng: 'File',
+        tr: 'dosya',
+    },
+    {
+        eng: 'Advise',
+        tr: 'önermek',
+    },
+    {
+        eng: 'Announce',
+        tr: 'duyurmak',
+    },
+    {
+        eng: 'Apologize',
+        tr: 'özür dilemek',
+    },
+    {
+        eng: 'Ask',
+        tr: 'sormak',
+    },
+    {
+        eng: 'Belive',
+        tr: 'inanmak',
+    },
+    {
+        eng: 'Dawn',
+        tr: 'şafak',
+    },
+    {
+        eng: 'Planet',
+        tr: 'gezegen',
+    },
+    {
+        eng: 'Space',
+        tr: 'uzay',
+    },
+    {
+        eng: 'Library',
+        tr: 'kütüphane',
+    },
+    {
+        eng: 'Science',
+        tr: 'bilim',
+    },
+    {
+        eng: 'Wheel',
+        tr: 'teker',
+    },
+    {
+        eng: 'Towel',
+        tr: 'havlu',
+    },
+    {
+        eng: 'Vein',
+        tr: 'damar',
+    },
+    {
+        eng: 'Love',
+        tr: 'aşk',
+    },
+    {
+        eng: 'Quote',
+        tr: 'alıntılamak',
+    },
+    {
+        eng: 'Remain',
+        tr: 'kalmak',
+    },
+    {
+        eng: 'Repat',
+        tr: 'tekrar etmek',
+    },
+    {
+        eng: 'Seperate',
+        tr: 'ayrı',
+    },
+    {
+        eng: 'Begin',
+        tr: 'başlamak',
+    },
+    {
+        eng: 'Behave',
+        tr: 'davranmak',
+    },
+    {
+        eng: 'Believe',
+        tr: 'inanmak',
+    },
+    {
+        eng: 'Belong',
+        tr: 'ait olmak',
+    },
+    {
+        eng: 'Bite',
+        tr: 'ısırmak',
+    },
+    {
+        eng: 'Blame',
+        tr: 'suçlamak',
+    },
+    {
+        eng: 'Bleed',
+        tr: 'kanamak',
+    },
+    {
+        eng: 'Bless',
+        tr: 'kutsamak',
+    },
+    {
+        eng: 'Blind',
+        tr: 'kör',
+    },
+    {
+        eng: 'Blow',
+        tr: 'patlatmak',
+    },
+    {
+        eng: 'Boil',
+        tr: 'kaynamak',
+    },
+    {
+        eng: 'Bomb',
+        tr: 'bomba',
+    },
+    {
+        eng: 'Booking',
+        tr: 'rezervasyon yapmak',
+    },
+    {
+        eng: 'Brake',
+        tr: 'fren yapmak',
+    },
+    {
+        eng: 'Break',
+        tr: 'kırmak',
+    },
+    {
+        eng: 'Breathe',
+        tr: 'nefes almak',
+    },
+    {
+        eng: 'Bring',
+        tr: 'getirmek',
+    },
+    {
+        eng: 'Build',
+        tr: 'inşa etmek',
+    },
+    {
+        eng: 'Resign',
+        tr: 'istifa etmek',
+    },
+    {
+        eng: 'Agriculture',
+        tr: 'tarım',
+    },
+    {
+        eng: 'Journey',
+        tr: 'seyahat',
+    },
+    {
+        eng: 'Wind',
+        tr: 'rüzgar',
+    },
+    {
+        eng: 'Anticipate',
+        tr: 'ummak',
+    },
+    {
+        eng: 'Foreign',
+        tr: 'yabancı',
+    },
+    {
+        eng: 'Attention',
+        tr: 'dikkat',
+    },
+    {
+        eng: 'Describe',
+        tr: 'betimlemek',
+    },
+    {
+        eng: 'Permission',
+        tr: 'izin',
+    },
+    {
+        eng: 'Charge',
+        tr: 'şarj',
+    },
+    {
+        eng: 'Box',
+        tr: 'kutu',
+    },
+    {
+        eng: 'Street',
+        tr: 'sokak',
+    },
+    {
+        eng: 'Side',
+        tr: 'yan',
+    },
+    {
+        eng: 'Main',
+        tr: 'ana',
+    },
+    {
+        eng: 'Concrete',
+        tr: 'beton',
+    },
+    {
+        eng: 'Cable',
+        tr: 'kablo',
+    },
+    {
+        eng: 'Canary',
+        tr: 'kanarya',
+    },
+    {
+        eng: 'Bull',
+        tr: 'boğa',
+    },
+    {
+        eng: 'Lip',
+        tr: 'dudak',
+    },
+    {
+        eng: 'Queue',
+        tr: 'sıra',
+    },
+    {
+        eng: 'Value',
+        tr: 'değer',
+    },
+    {
+        eng: 'Eagle',
+        tr: 'kartal',
+    },
+    {
+        eng: 'Admit',
+        tr: 'kabul etmek',
+    },
+    {
+        eng: 'Lazy',
+        tr: 'tembel',
+    },
+    {
+        eng: 'Coffy',
+        tr: 'kahve',
+    },
+    {
+        eng: 'Island',
+        tr: 'ada',
+    },
+    {
+        eng: 'Treasure',
+        tr: 'hazine',
+    },
+    {
+        eng: 'Pitch',
+        tr: 'saha',
+    },
+    {
+        eng: 'Chant',
+        tr: 'tezahürat',
+    },
+    {
+        eng: 'Faculty',
+        tr: 'fakülte',
+    },
+    {
+        eng: 'Religion',
+        tr: 'din',
+    },
+    {
+        eng: 'Engine',
+        tr: 'motor',
+    },
+    {
+        eng: 'Excellent',
+        tr: 'mükemmel',
+    },
+    {
+        eng: 'Own',
+        tr: 'sahip olmak',
+    },
+    {
+        eng: 'Borrow',
+        tr: 'ödünç almak',
+    },
+    {
+        eng: 'Idea',
+        tr: 'fikir',
+    },
+    {
+        eng: 'Research',
+        tr: 'araştırma',
+    },
+    {
+        eng: 'Appliaction',
+        tr: 'başvuru',
+    },
+    {
+        eng: 'Charming',
+        tr: 'çekici',
+    },
+    {
+        eng: 'Calculator',
+        tr: 'hesap makinası',
+    },
+    {
+        eng: 'Profit',
+        tr: 'kar',
+    },
+    {
+        eng: 'Quantity',
+        tr: 'miktar',
+    },
+    {
+        eng: 'Fuel',
+        tr: 'yakıt',
+    },
+    {
+        eng: 'Phone',
+        tr: 'telefon',
+    },
+    {
+        eng: 'Ambassador',
+        tr: 'elçi',
+    },
+    {
+        eng: 'Surprised',
+        tr: 'şaşırmış',
+    },
+    {
+        eng: 'Postpone',
+        tr: 'ertelemek',
+    },
+    {
+        eng: 'Hurt',
+        tr: 'incitmek',
+    },
+    {
+        eng: 'Heart',
+        tr: 'kalp',
+    },
+    {
+        eng: 'Regular',
+        tr: 'sürekli',
+    },
+    {
+        eng: 'Boring',
+        tr: 'sıkıcı',
+    },
+    {
+        eng: 'Shake',
+        tr: 'sallamak',
+    },
+    {
+        eng: 'Sin',
+        tr: 'günah',
+    },
+    {
+        eng: 'Spell',
+        tr: 'hecelemek',
+    },
+    {
+        eng: 'Supply',
+        tr: 'tedarik',
+    },
+    {
+        eng: 'Worry',
+        tr: 'endişelenmek',
+    },
+    {
+        eng: 'Yell',
+        tr: 'bağırmak',
+    },
+    {
+        eng: 'Marry',
+        tr: 'evlenmek',
+    },
+    {
+        eng: 'Jail',
+        tr: 'hapis',
+    },
+    {
+        eng: 'Cold',
+        tr: 'soğuk',
+    },
+    {
+        eng: 'Divorce',
+        tr: 'boşanmak',
+    }
+]
+// Kelimeler
+
+var engWord = document.getElementById('eng-word')
+var trWord = document.getElementById('tr-word')
+var randomIndex = Math.floor(Math.random() * words.length)
+var randomEng = words[randomIndex].eng
+var randomTr = words[randomIndex].tr
+engWord.textContent = randomEng
+console.log(Math.floor(Math.random() * words.length))
+var correctIncorrect = document.getElementById('correct-incorrect')
+var barOne = document.getElementById('bar1')
+var barTwo = document.getElementById('bar2')
+var barThree = document.getElementById('bar3')
+var barFour = document.getElementById('bar4')
+var barFive = document.getElementById('bar5')
+var barSix = document.getElementById('bar6')
+var barSeven = document.getElementById('bar7')
+var barEight = document.getElementById('bar8')
+var barNine = document.getElementById('bar9')
+var barTen = document.getElementById('bar10')
+var toggleContainer = document.getElementById('tg')
+
+// Puan Sistemi
+var puan = 0
+
+function sonuc() {
+    if (trWord.value == randomTr) {
+        console.log('tebrikler')
+        var randomIndex = Math.floor(Math.random() * words.length)
+        randomEng = words[randomIndex].eng
+        randomTr = words[randomIndex].tr
+        engWord.textContent = randomEng
+        console.log(randomIndex)
+        trWord.value = ''
+        // correct-incorrect
+        correctIncorrect.style.opacity = (1)
+        correctIncorrect.style.marginTop = ('1em')
+        correctIncorrect.style.transition = ('0.5s ease')
+        setTimeout(() => {
+            correctIncorrect.style.opacity = (0)
+            correctIncorrect.style.marginTop = ('-1em')
+            correctIncorrect.style.transition = ('0.5s ease')
+        }, 1500);
+
+        // Puan Sistemi
+
+        puan += 15
+        console.log('puanınız : ' + puan)
+        if (Number(puan) > 50) {
+            barOne.style.background = ('black')
+            barOne.style.transition = ('0.4s ease')
+        } else {
+            barOne.style.background = ('gray')
+        }
+        if (Number(puan) > 100) {
+            barTwo.style.background = ('black')
+            barTwo.style.transition = ('0.4s ease')
+        } else {
+            barTwo.style.background = ('gray')
+        }
+        if (Number(puan) > 175) {
+            barThree.style.background = ('black')
+            barThree.style.transition = ('0.4s ease')
+        } else {
+            barThree.style.background = ('gray')
+        }
+        if (Number(puan) > 250) {
+            barFour.style.background = ('black')
+            barFour.style.transition = ('0.4s ease')
+        } else {
+            barFour.style.background = ('gray')
+        }
+        if (Number(puan) > 375) {
+            barFive.style.background = ('black')
+            barFive.style.transition = ('0.4s ease')
+        } else {
+            barFive.style.background = ('gray')
+        }
+        if (Number(puan) > 500) {
+            barSix.style.background = ('black')
+            barSix.style.transition = ('0.4s ease')
+        } else {
+            barSix.style.background = ('gray')
+        }
+        if (Number(puan) > 625) {
+            barSeven.style.background = ('black')
+            barSeven.style.transition = ('0.4s ease')
+        } else {
+            barSeven.style.background = ('gray')
+        }
+        if (Number(puan) > 750) {
+            barEight.style.background = ('black')
+            barEight.style.transition = ('0.4s ease')
+        } else {
+            barEight.style.background = ('gray')
+        }
+        if (Number(puan) > 875) {
+            barNine.style.background = ('black')
+            barNine.style.transition = ('0.4s ease')
+        } else {
+            barNine.style.background = ('gray')
+        }
+        if (Number(puan) > 1000) {
+            barTen.style.background = ('black')
+            barTen.style.transition = ('0.4s ease')
+            congrats()
+        } else {
+            barTen.style.background = ('gray')
+        }
+
+    } else {
+        console.log('yanlış sonuç')
+        correctIncorrect.style.opacity = (1)
+        correctIncorrect.style.marginTop = ('1em')
+        correctIncorrect.style.transition = ('0.5s ease')
+        correctIncorrect.style.backgroundColor = ('red')
+        correctIncorrect.textContent = ('Yanlış Bildiniz, tekrar deneyiniz. -10 Puan')
+        setTimeout(() => {
+            correctIncorrect.style.opacity = (0)
+            correctIncorrect.style.marginTop = ('-1em')
+            correctIncorrect.style.transition = ('0.5s ease')
+            correctIncorrect.style.backgroundColor = ('')
+        }, 1500);
+        setTimeout(() => {
+            correctIncorrect.textContent = ('Doğru Bildiniz! +15 Puan')
+        }, 1750);
+        puan -= 10
+        console.log('puanınız : ' + puan)
+
+        // Puan
+
+        if (Number(puan) > 50) {
+            barOne.style.background = ('black')
+            barOne.style.transition = ('0.4s ease')
+        } else {
+            barOne.style.background = ('gray')
+        }
+        if (Number(puan) > 100) {
+            barTwo.style.background = ('black')
+            barTwo.style.transition = ('0.4s ease')
+        } else {
+            barTwo.style.background = ('gray')
+        }
+        if (Number(puan) > 175) {
+            barThree.style.background = ('black')
+            barThree.style.transition = ('0.4s ease')
+        } else {
+            barThree.style.background = ('gray')
+        }
+        if (Number(puan) > 250) {
+            barFour.style.background = ('black')
+            barFour.style.transition = ('0.4s ease')
+        } else {
+            barFour.style.background = ('gray')
+        }
+        if (Number(puan) > 375) {
+            barFive.style.background = ('black')
+            barFive.style.transition = ('0.4s ease')
+        } else {
+            barFive.style.background = ('gray')
+        }
+        if (Number(puan) > 500) {
+            barSix.style.background = ('black')
+            barSix.style.transition = ('0.4s ease')
+        } else {
+            barSix.style.background = ('gray')
+        }
+        if (Number(puan) > 625) {
+            barSeven.style.background = ('black')
+            barSeven.style.transition = ('0.4s ease')
+        } else {
+            barSeven.style.background = ('gray')
+        }
+        if (Number(puan) > 750) {
+            barEight.style.background = ('black')
+            barEight.style.transition = ('0.4s ease')
+        } else {
+            barEight.style.background = ('gray')
+        }
+        if (Number(puan) > 875) {
+            barNine.style.background = ('black')
+            barNine.style.transition = ('0.4s ease')
+        } else {
+            barNine.style.background = ('gray')
+        }
+        if (Number(puan) > 1000) {
+            barTen.style.background = ('black')
+            barTen.style.transition = ('0.4s ease')
+        } else {
+            barTen.style.background = ('gray')
+        }
+    }
+}
+
+// Background Picker
+
+var pikachu = document.getElementById('pikachubg').style.background = 'url(pikachu.jpg)'
+function bgp() {
+    toggleContainer.style.background = (pikachu)
+    toggleContainer.style.backgroundPosition = ('center')
+    toggleContainer.style.backgroundSize = ('cover')
+}
+var space = document.getElementById('spacebg').style.background = 'url(space.jpg)'
+function bgs() {
+    toggleContainer.style.background = (space)
+    toggleContainer.style.backgroundPosition = ('center')
+    toggleContainer.style.backgroundSize = ('cover')
+}
+var illu = document.getElementById('illubg').style.background = 'url(illustration.jpg)'
+function bgi() {
+    toggleContainer.style.background = (illu)
+    toggleContainer.style.backgroundSize = ('cover')
+}
+function bgb() {
+    toggleContainer.style.background = ('#211d1d')
+}
+
+var congrat = document.getElementById('congrat')
+function congrats() {
+    congrat.style.transform = ('translateY(300px)')
+    congrat.style.opacity = (1)
+    congrat.style.transition = ('1s ease')
+}
+function restart() {
+    location.reload(true)
+}
